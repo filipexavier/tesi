9f15201026cc5e7716075c140211e4124c8442f9
filipexts <filipexts@gmail.com>
2014-11-04 12:55:58 -0200

    share project

---
+++ src/main/java/br/ufrj/dcc/tesi/crawlers/GloboCrawler.java
@@ -0,0 +1,7 @@
+package br.ufrj.dcc.tesi.crawlers;
+
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+
+public class GloboCrawler extends WebCrawler {
+
+}
---
+++ src/main/java/br/ufrj/dcc/tesi/main/Tesi.java
@@ -0,0 +1,5 @@
+package br.ufrj.dcc.tesi.main;
+
+public class Tesi {
+
+}
